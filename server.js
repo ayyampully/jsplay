91ea8b2486e3e050ecec3cb73184560feb4f32a5
Rohith <rohith.ayyampully.com>
2016-05-28 13:36:03 +0530

    adding node server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-server.js
+const express = require('express');
+const app = express();
+
+app.use('/', express.static(__dirname+'/app'));
+
+const port = process.env.PORT || 3030;
+app.listen(port);
+console.log('Server is at port:'+port);
