51d178e67629a86ad4e4b4f72c7387b7dbe8783e
Rahul A <Rahul A>
2016-11-12 22:02:30 +0000

    changing the port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/', express.static(__dirname+'/app'));
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3031;
 app.listen(port);
 console.log('Server is at port:'+port);
